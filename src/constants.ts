2e98bf09dc270e174f5545e7e120a91173f107a0
Fluffyalien1422 <shawnbhumbla2@gmail.com>
2024-11-03 10:12:44 -0800

    refactor: change `MAX_MESSAGE_LENGTH` to 2047

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,9 @@
+// actual max is 2048, but we need to allow one character for the ipc type flag
 /**
  * Max serialized message length.
  */
-export const MAX_MESSAGE_LENGTH = 2048;
+export const MAX_MESSAGE_LENGTH = 2047;
+
 /**
  * Max namespace length.
  */

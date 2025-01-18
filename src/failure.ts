44923247f4e1a384e6b0daef44e7d1f7764ecff4
Fluffyalien1422 <shawnbhumbla2@gmail.com>
2025-01-18 15:07:54 -0800

    refactor: increase STREAM_MESSAGE_PADDING + more comments + small changes

--- src/failure.ts
+++ src/failure.ts
@@ -4,9 +4,9 @@
  * This will automatically be logged as a warning if the listener is not invoked because values can only be returned on invoke.
  * @example
  * ```ts
- * ipc.registerListener("example:alwaysError", () => {
+ * router.registerListener("example:alwaysError", () => {
  *  // do something ...
- *  return new ipc.Failure("An error has occured.");
+ *  return new Failure("An error has occured.");
  * })
  * ```
  */
@@ -14,5 +14,41 @@ export class Failure extends Error {
   constructor(message: string) {
     super(message);
     this.name = "Failure";
+  }
+
+  /**
+   * Call a function and return a {@link Failure} if it throws an error.
+   */
+  static try<TArgs extends unknown[], TReturn>(
+    func: (...args: TArgs) => TReturn,
+    ...args: TArgs
+  ): TReturn | Failure {
+    let result: TReturn;
+    let err: string | undefined;
+
+    try {
+      result = func(...args);
+    } catch (e) {
+      err = String(e);
+    }
+
+    if (err !== undefined) {
+      return new Failure(err);
+    }
+
+    return result!;
+  }
+
+  /**
+   * Wrap a function in an another function that calls the inner function using {@link Failure.try}.
+   * This is the same as doing:
+   * ```js
+   * const wrapped = (...args) => Failure.try(func, ...args);
+   * ```
+   */
+  static tryWrap<TArgs extends unknown[], TReturn>(
+    func: (...args: TArgs) => TReturn
+  ): (...args: TArgs) => TReturn | Failure {
+    return (...args: TArgs) => Failure.try(func, ...args);
   }
 }
